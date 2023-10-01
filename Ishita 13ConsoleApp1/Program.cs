c4469bbc90120b3cd6b30a90d8807e1bdb76bdbb
A <A@ANUJ>
2023-09-30 21:42:02 -0400

    Add project files.

---
+++ Ishita 13ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ishita_13ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, t = 9;
+            int sum = 0;
+
+            Console.Write("\n\n");
+            Console.Write("Display the sum of the series [ 9 + 99 + 999 + 9999 ...]:\n");
+            Console.Write("-----------------------------------------------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Input the number or terms :");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (i = 1; i <= n; i++)
+            {
+                sum += t;
+                Console.Write("{0}   ", t);
+                t = t * 10 + 9;
+            }
+            Console.Write("\nThe sum of the saries = {0} \n", sum);
+        
+    
+        }
+    }
+}
